b8a1b37a18a7962b1165a582b9a86d386a009add
Victor Kildahl Petersen <vkp@jysk.com>
2022-05-30 10:31:58 +0200

    test 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c981f4f51e4e53a4"; }
+/******/ 		__webpack_require__.h = function() { return "85d74686b3614c57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
