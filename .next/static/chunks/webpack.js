becb3964c90bdc58287083ad15f7a756e147952a
Victor Kildahl Petersen <vkp@jysk.com>
2022-05-30 13:26:52 +0200

    fetching data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "20ced26db939ef8a"; }
+/******/ 		__webpack_require__.h = function() { return "5855690265bd0e19"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Products/index.tsx
+++ pages/Products/index.tsx
@@ -1,3 +1,43 @@
-export default function Products() {
-  return <div>Products</div>;
+import Image from "next/image";
+
+type ProductResp = {
+  products: [
+    {
+      id: number;
+      title: string;
+      price: string;
+      category: string;
+      description: string;
+      image: string;
+    }
+  ];
+};
+
+export default function Products({ products }: ProductResp) {
+  return (
+    <div>
+      <h1>Products</h1>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h1>{product.title}</h1>
+          <h1>{product.price}</h1>
+          <Image
+            src={product.image}
+            alt="Vercel Logo"
+            width={500}
+            height={500}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const req = await fetch("https://fakestoreapi.com/products");
+  const data = await req.json();
+
+  return {
+    props: { products: data },
+  };
 }
